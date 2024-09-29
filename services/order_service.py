50b8c7075f3d785edb733138fd76b26db60d2b5c
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2024-09-29 17:15:41 +0300

    Edit services

--- services/order_service.py
+++ services/order_service.py
@@ -2,6 +2,8 @@ from fastapi import Depends
 from repositories.order_repository import OrderRepository, get_order_repository
 from repositories.product_repository import ProductRepository, get_product_repository
 from database.models import Order, OrderItem, OrderStatus
+from exceptions import NotEnoughStockException, NotFoundException, InvalidStatusException, NotFoundItemsException
+from schemes.order_scheme import OrderCreate
 
 class OrderService:
     def __init__(self, order_repo: OrderRepository, product_repo):
@@ -9,33 +11,38 @@ class OrderService:
         self.product_repo = product_repo
 
     # Creating a new order
-    async def create_order(self, order_data: dict):
+    async def create_order(self, order_data: OrderCreate):
         order = Order()
-        for item in order_data["items"]:
-            product = await self.product_repo.get(item["product_id"])
-            if product.stock < item["quantity"]:
-                raise ValueError(f"Not enough stock for product {product.name}")
-            product.stock -= item["quantity"]
-            order_item = OrderItem(product_id=product.id, quantity=item["quantity"])
+        for item in order_data.items:
+            product = await self.product_repo.get(item.product_id)
+            if product.stock < item.quantity:
+                raise NotEnoughStockException(item.product_id, product.stock)
+            product.stock -= item.quantity
+            order_item = OrderItem(product_id=product.id, quantity=item.quantity)
             order.items.append(order_item)
         return await self.order_repo.create(order)
 
     # Fetching the list of all orders
     async def list_orders(self):
-        return await self.order_repo.list()
+        orders = await self.order_repo.list()
+        if not orders:
+            raise NotFoundItemsException("orders")
+        return orders
 
     # Fetching a specific order by ID
     async def get_order(self, order_id: int):
         order = await self.order_repo.get(order_id)
         if not order:
-            raise ValueError(f"Order with ID {order_id} not found")
+            raise NotFoundException("order", order_id)
         return order
 
     # Updating the status of an order
     async def update_order_status(self, order_id: int, status: str):
         order = await self.get_order(order_id)
+        if not order:
+            raise NotFoundException("order", order_id)
         if status not in OrderStatus.__members__:
-            raise ValueError(f"Invalid order status: {status}")
+            raise InvalidStatusException(status)
         return await self.order_repo.update_status(order_id, OrderStatus[status])
 
 def get_order_service(
--- services/product_service.py
+++ services/product_service.py
@@ -2,28 +2,42 @@ from fastapi import Depends
 from repositories.product_repository import get_product_repository
 from repositories.product_repository import ProductRepository
 from schemes.product_scheme import ProductCreate, ProductUpdate
+from database.models import Product
+from exceptions import NotFoundException, NotFoundItemsException
 
 class ProductService:
     def __init__(self, product_repo: ProductRepository):
         self.product_repo = product_repo
 
     async def create_product(self, product_data: ProductCreate):
-        return await self.product_repo.create(product_data.dict())
+        data = Product(**product_data.dict())
+        return await self.product_repo.create(data)
 
     async def list_products(self):
-        return await self.product_repo.list()
+        products = await self.product_repo.list()
+        if not products:
+            raise NotFoundItemsException("products")
+        return products
 
     async def get_product(self, product_id: int):
         product = await self.product_repo.get(product_id)
         if not product:
-            raise ValueError(f"Product with ID {product_id} not found")
+            # raise ValueError(f"Product with ID {product_id} not found")
+            raise NotFoundException("product", product_id)
         return product
 
     async def update_product(self, product_id: int, product_data: ProductUpdate):
-        return await self.product_repo.update(product_id, product_data.dict(exclude_unset=True))
+        data = product_data.dict(exclude_unset=True)
+        product = await self.product_repo.update(product_id, data)
+        if not product:
+            raise NotFoundException("product", product_id)
+        return product
 
     async def delete_product(self, product_id: int):
-        return await self.product_repo.delete(product_id)
+        product = await self.product_repo.delete(product_id)
+        if not product:
+            raise NotFoundException("product", product_id)
+        return product
 
 
 def get_product_service(product_repo: ProductRepository = Depends(get_product_repository)):
