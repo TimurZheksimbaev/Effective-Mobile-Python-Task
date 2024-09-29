3f67d1fa0693f874ab7a73c729155258baa236c7
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2024-09-29 12:41:12 +0300

    Added product and order repositories

---
+++ repositories/order_repository.py
@@ -0,0 +1,33 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from database.models import Order
+from database.connect import get_db
+from sqlalchemy import Depends
+
+class OrderRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def create(self, order: Order):
+        self.db.add(order)
+        await self.db.commit()
+        await self.db.refresh(order)
+        return order
+
+    async def get(self, order_id: int):
+        result = await self.db.execute(select(Order).where(Order.id == order_id))
+        return result.scalar_one_or_none()
+
+    async def list(self):
+        result = await self.db.execute(select(Order))
+        return result.scalars().all()
+
+    async def update_status(self, order_id: int, status):
+        order = await self.get(order_id)
+        if order:
+            order.status = status
+            await self.db.commit()
+        return order
+
+def get_order_repository(db: AsyncSession = Depends(get_db)):
+    return OrderRepository(db)
---
+++ repositories/product_repository.py
@@ -0,0 +1,41 @@
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from database.models import Product
+from database.connect import get_db
+from fastapi import Depends
+
+class ProductRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def create(self, product: Product):
+        self.db.add(product)
+        await self.db.commit()
+        await self.db.refresh(product)
+        return product
+
+    async def get(self, product_id: int):
+        result = await self.db.execute(select(Product).where(Product.id == product_id))
+        return result.scalar_one_or_none()
+
+    async def list(self):
+        result = await self.db.execute(select(Product))
+        return result.scalars().all()
+
+    async def update(self, product_id: int, product_data: dict):
+        product = await self.get(product_id)
+        if product:
+            for key, value in product_data.items():
+                setattr(product, key, value)
+            await self.db.commit()
+        return product
+
+    async def delete(self, product_id: int):
+        product = await self.get(product_id)
+        if product:
+            await self.db.delete(product)
+            await self.db.commit()
+        return product
+
+def get_product_repository(db: AsyncSession = Depends(get_db)):
+    return ProductRepository(db)
