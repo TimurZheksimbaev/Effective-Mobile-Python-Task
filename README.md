255c8e9b269fc7ce6571a05d67cec34f01a30803
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2024-09-29 22:14:10 +0300

    Added readme and .env.example

---
+++ README.md
@@ -0,0 +1,26 @@
+# Задание для Effective Mobile
+# Просто API для управления складом. Написаные основные CRUD операции для товаров и заказов.
+
+## Для запуска приложения сначала склоинруйте репозиторий
+```bash
+git clone https://github.com/TimurZheksimbaev/Effective-Mobile-Python-Task.git
+cd Effective-Mobile-Python-Task
+```
+
+## Потом скопируйте .env.local в .env
+```bash
+cp .env.example .env
+```
+## И добавьте .env еще в директорию configs
+
+### Для запуска используйте
+```bash
+python main.py
+```
+
+## Или
+```bash
+docker compose up --build
+```
+
+## Документация http://localhost:8000/docs
--- main.py
+++ main.py
@@ -10,28 +10,23 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# Add CORS Middleware (Optional, based on your needs)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust allowed origins for CORS
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Include the main API router that contains both products and orders
 app.include_router(api_router)
 
 
-# Add startup and shutdown events if needed (Optional)
 @app.on_event("startup")
 async def startup_event():
-    # If you need to create database tables automatically
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
 
-# Initialize and run the app if this file is executed directly
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
